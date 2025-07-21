d18333c83ba19399d57bf97078f6f2f9610c636d
F1OMER <70028022+F10MER@users.noreply.github.com>
2025-07-22 02:29:10 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react( ), tailwindcss()],
-  base: '/', // ← Добавьте эту строку!
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
